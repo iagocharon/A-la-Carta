66a721e090d71a1ae76e967a8b4d964b58261842
iagocharon <iago@iagocharon.com>
2022-10-19 22:08:48 -0300

    Updated environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  authURL: 'http://challenge-react.alkemy.org/',
+  platoURL: 'https://api.spoonacular.com/recipes/',
+  information: '/information?',
+  searchURL: 'https://api.spoonacular.com/recipes/complexSearch?query=',
+  number: '&number=8&',
+  APIKey: 'apiKey=bd55ae92df814e6487a46786aa7c885d',
 };
